1a99dcb9f5f8956396913e347009d9e960011699
Vaiva Kazanaviciute <vkazanav@student.42.fr>
2025-10-13 18:51:17 +0200

    fixup! fix anim timeout cancellation

--- frontend/src/pages/Welcome.ts
+++ frontend/src/pages/Welcome.ts
@@ -7,7 +7,7 @@ const KEY3 = ["-translate-y-2/3", "opacity-0", "duration-300"];
 
 export default class WelcomePage extends Page {
   playButton = new Paragraph("Play →").withId("play-btn")
-    .class("font-bold text-2xl mt-10 transition duration-1300 select-none")
+    .class("font-bold text-2xl mt-10 transition duration-1300")
     .class(KEY1.join(" "));
 
   titleCard = new Div([
@@ -17,7 +17,7 @@ export default class WelcomePage extends Page {
     new Paragraph("Pong. Delivered to your doorstep."),
     this.playButton,
   ]).class("absolute top-1/2 left-1/2 transform -translate-x-1/2")
-    .class("transition duration-1300 flex flex-col items-center")
+    .class("transition duration-1300 flex flex-col items-center select-none")
     .class(KEY1.join(" "))
     .withId("title-card");
 
@@ -30,21 +30,24 @@ export default class WelcomePage extends Page {
   }
 
   bindEvents() {
-    setTimeout(() => {
-      this.titleCard.byId()?.classList.remove(...KEY1);
-      this.titleCard.byId()?.classList.add(...KEY2);
-    }, 200);
-    setTimeout(() => {
-      this.playButton.byId()?.classList.remove(...KEY1);
-      this.playButton.byId()?.classList.add(...KEY2);
-    }, 1500);
+    const anims = [
+      setTimeout(() => {
+        this.titleCard.byId()?.classList.remove(...KEY1);
+        this.titleCard.byId()?.classList.add(...KEY2);
+      }, 200),
+      setTimeout(() => {
+        this.playButton.byId()?.classList.remove(...KEY1);
+        this.playButton.byId()?.classList.add(...KEY2);
+      }, 1500)
+    ];
 
     const cb = () => {
       this.titleCard.byId()?.classList.remove(...KEY2, "duration-1300");
       this.titleCard.byId()?.classList.add(...KEY3);
+      anims.forEach((a) => clearTimeout(a));
       setTimeout(() => this.router.navigate("play"), 300);
       this.playButton.byId()?.removeEventListener('click', cb);
     };
-    this.playButton.byId()?.addEventListener('click', cb);
+    this.titleCard.byId()?.addEventListener('click', cb);
   }
 }
