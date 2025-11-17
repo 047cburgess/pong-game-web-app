fe243b36be59fa1bce3b765ffe2f988bc0933217
Vaiva Kazanaviciute <vkazanav@student.42.fr>
2025-11-17 21:08:58 +0100

    123

--- backend/game_service/src/config.ts
+++ backend/game_service/src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  PORT: process.env['LISTEN_PORT'] ? parseInt(process.env['LISTEN_PORT'], 10) : 3001,
+  PORT: process.env['LISTEN_PORT'] ? parseInt(process.env['LISTEN_PORT'], 10) : 3000,
   NODE_ENV: process.env['NODE_ENV'] || 'development',
   LOG_LEVEL: process.env['LOG_LEVEL'] || 'info',
 };
